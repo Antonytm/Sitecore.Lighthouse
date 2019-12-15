d76b7527205488a9c8a4c53299be1504c8eb7025
Antonytm <antontishchenko@gmail.com>
2019-12-15 10:10:36 +0200

    Test code to make sure that SonarCloud works

---
+++ src/Foundation/Lighthouse/code/TestClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Foundation.Lighthouse
+{
+    public class TestClass
+    {
+        public bool SonarTest { get; set; }
+        public void Test()
+        {
+            if (SonarTest)
+            {
+                Console.Write("Success");
+            }
+        }
+    }
+}
