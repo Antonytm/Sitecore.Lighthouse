afbce0b084262dfd4ff9c9305f7b8589ee232edf
Antonytm <antontishchenko@gmail.com>
2019-12-17 08:00:46 +0200

    Adding command

---
+++ src/Foundation/Lighthouse/code/Commands/Run.cs
@@ -0,0 +1,16 @@
+﻿using Sitecore.Shell.Framework.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Foundation.Lighthouse.Commands
+{
+    public class Run : Command
+    {
+        public override void Execute(CommandContext context)
+        {
+            Sitecore.Context.ClientPage.ClientResponse.Alert("Testing my button");
+        }
+    }
+}
