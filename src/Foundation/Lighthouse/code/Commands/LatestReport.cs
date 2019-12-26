461be155bdfed0b4b23bcf6465e1de79124090ad
Antonytm <antontishchenko@gmail.com>
2019-12-26 08:46:41 +0200

    Adding more buttons

---
+++ src/Foundation/Lighthouse/code/Commands/LatestReport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Sitecore.Shell.Framework.Commands;
+
+namespace Foundation.Lighthouse.Commands
+{
+    public class LatestReport : Command
+    {
+        private readonly LighthouseRunner _lighthouseRunner;
+        public LatestReport()
+        {
+            _lighthouseRunner = new LighthouseRunner();
+        }
+        public override void Execute(CommandContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Foundation/Lighthouse/code/Commands/RunAll.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Sitecore.Shell.Framework.Commands;
+
+namespace Foundation.Lighthouse.Commands
+{
+    public class RunAll : Command
+    {
+        private readonly LighthouseRunner _lighthouseRunner;
+        public RunAll()
+        {
+            _lighthouseRunner = new LighthouseRunner();
+        }
+        public override void Execute(CommandContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
