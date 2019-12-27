c3026950b54405955614f8ff665f4f99ae95297e
Antonytm <antontishchenko@gmail.com>
2019-12-27 07:49:39 +0200

    Open latest report implementation

--- src/Foundation/Lighthouse/code/Commands/LatestReport.cs
+++ src/Foundation/Lighthouse/code/Commands/LatestReport.cs
@@ -1,18 +1,36 @@
-﻿using System;
+﻿using System.IO;
+using System.Linq;
+using System.Web;
 using Sitecore.Shell.Framework.Commands;
+using Sitecore.Web.UI.Sheer;
 
 namespace Foundation.Lighthouse.Commands
 {
     public class LatestReport : Command
     {
-        private readonly LighthouseRunner _lighthouseRunner;
+        private readonly Paths _paths;
         public LatestReport()
         {
-            _lighthouseRunner = new LighthouseRunner();
+            _paths = new Paths();
         }
         public override void Execute(CommandContext context)
         {
-            throw new NotImplementedException();
+            var item = context.Items[0];
+            var directory = new DirectoryInfo(_paths.GetReportsPath(item));
+            var latestFile = directory.GetFiles("*.html")
+                .OrderByDescending(f => f.LastWriteTime)
+                .FirstOrDefault();
+            if (latestFile != null)
+            {
+                //Do not disclose internal server configuration to user
+                var url = $"{HttpContext.Current.Request.Url.Scheme}://{HttpContext.Current.Request.Url.Host}/api/sitecore/lighthouse/ShowFileContent?filename={latestFile.Name}&database={item.Database}&itemId={item.ID}";
+                SheerResponse.ShowModalDialog(new ModalDialogOptions(url) { Response = false });
+            }
+            else
+            {
+                SheerResponse.ShowError("Latest Lighthouse report was not found.",
+                    "Please, try to run report creation before trying to open it.");
+            }
         }
     }
 }
---
+++ src/Foundation/Lighthouse/code/Controllers/LighthouseController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Foundation.Lighthouse.Controllers
+{
+    public class LighthouseController : Controller
+    {
+        private readonly Paths _paths;
+        public LighthouseController()
+        {
+            _paths = new Paths();
+        }
+        public ActionResult ShowFileContent(string filename, string itemId, string database)
+        {
+            try
+            {
+                var item = Sitecore.Data.Database.GetDatabase(database).GetItem(itemId);
+                var filepath = $"{_paths.GetReportsPath(item)}\\{filename}";
+                return new ContentResult() {Content = System.IO.File.ReadAllText(filepath)};
+            }
+            catch (Exception ex)
+            {
+                Sitecore.Diagnostics.Log.Error($"Lighthouse: was not able find file:'{filename}', itemId:{itemId}, database:{database}",ex, this);
+                return new ContentResult(){Content = "Sorry, exception happened. Please check Sitecore logs."};
+            }
+        }
+    }
+}
