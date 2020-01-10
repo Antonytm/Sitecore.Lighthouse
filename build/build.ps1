9c318c3812832dbf4ac77c50b80e0e8a574382be
Antonytm <antontishchenko@gmail.com>
2020-01-10 09:25:19 +0200

    Squashed commit of the following:
    commit bf3924a5397527313d6c1f458132dff66f5a10d9
    Author: Antonytm <antontishchenko@gmail.com>
    Date:   Fri Jan 10 09:07:21 2020 +0200
    
        Include lighthouse tools into package: put them outside of bin folder
    
    commit 611f58c5c07d74132a5a085d052bb297db5a8f8b
    Author: Antonytm <antontishchenko@gmail.com>
    Date:   Fri Jan 10 08:35:09 2020 +0200
    
        Include lighthouse tools into package: allow spaces in path
    
    commit 7da66db62c4abaef27422bc88cb67a4456135601
    Author: Antonytm <antontishchenko@gmail.com>
    Date:   Fri Jan 10 08:25:12 2020 +0200
    
        Include lighthouse tools into package: updated path
    
    commit 7acb986d315b61a60b688bad48d983429dca176c
    Author: Antonytm <antontishchenko@gmail.com>
    Date:   Fri Jan 10 08:16:30 2020 +0200
    
        Corrected path, include tools to package
    
    commit 13ddfb252e16db5c5aecd0a6612e9a1b155ee398
    Author: Antonytm <antontishchenko@gmail.com>
    Date:   Fri Jan 10 08:09:42 2020 +0200
    
        Update build: include lighthouse tools into package

--- build/build.ps1
+++ build/build.ps1
@@ -10,11 +10,14 @@ New-Item -Name build\package -ItemType directory
 New-Item -Name build\artifacts -ItemType directory
 New-Item -Name build\package\Data -ItemType directory
 New-Item -Name build\package\bin -ItemType directory
-New-Item -Name build\package\solr -ItemType directory
 New-Item -Name build\package\App_Config\Include\Foundation -ItemType directory
+New-Item -Name build\package\App_Data\Lighthouse\Tools -ItemType directory
+New-Item -Name build\package\App_Data\Lighthouse\Reports -ItemType directory
+
 
 Copy-Item .\src\Foundation\Lighthouse\code\bin\Foundation.Lighthouse* .\build\package\bin
 Copy-Item .\src\Foundation\Lighthouse\code\App_Config\Include\Foundation\Foundation.Lighthouse.config .\build\package\App_Config\Include\Foundation
+Copy-Item ".\lighthouse tools\*" .\build\package\App_Data\Lighthouse
 Copy-Item .\src\Foundation\Lighthouse\serialization\* .\build\package\Data -recurse
 
 $packageCmd = "Sitecore.Courier.Runner.exe -t build\package -o build\artifacts\sitecore.lighthouse." + $version + ".update -r"
