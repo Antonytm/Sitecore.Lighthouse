48180ba2556f5636c67f557fb73ead5bd96665f4
Antonytm <antontishchenko@gmail.com>
2020-02-13 07:19:56 +0200

    Saving summary for all sites

---
+++ src/Foundation/Lighthouse/code/Models/SiteSummary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Foundation.Lighthouse.Models
+{
+    public class SiteSummary
+    {
+        public string WorstItems { get; set; }
+        public string BestItems { get; set; }
+        public string Logs { get; set; }
+    }
+}
